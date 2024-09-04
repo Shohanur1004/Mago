0462d39207fd410b92b6f810e9f40e6d2410ace8
Shohanur1004 <97248001+Shohanur1004@users.noreply.github.com>
2024-09-04 06:37:59 +0600

    Update get-visitors-db-table.php

--- get-visitors-db-table.php
+++ get-visitors-db-table.php
@@ -1,5 +1,5 @@
 <?php
-// File path to user_logs.txt
+// File path to user_log.txt
 $filename = 'user_log.txt';
 
 // Check if the file exists
@@ -12,12 +12,12 @@ if (file_exists($filename)) {
         echo "No data to display.";
     } else {
         echo '<table border="1">';
-        
+
         // Assuming the first line contains column headers
         $headers = str_getcsv(array_shift($lines));
         echo '<tr>';
         foreach ($headers as $header) {
-            echo '<th>' . htmlspecialchars($header) . '</th>';
+            echo '<th>' . htmlspecialchars($header, ENT_QUOTES, 'UTF-8') . '</th>';
         }
         echo '</tr>';
 
@@ -26,7 +26,7 @@ if (file_exists($filename)) {
             $data = str_getcsv($line);
             echo '<tr>';
             foreach ($data as $field) {
-                echo '<td>' . htmlspecialchars($field) . '</td>';
+                echo '<td>' . htmlspecialchars($field, ENT_QUOTES, 'UTF-8') . '</td>';
             }
             echo '</tr>';
         }
